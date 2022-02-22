06acdc88598f972490951889628e0d96bd0c1a6e
zheneva <mr_zhenkins@mail.ru>
2022-02-22 21:54:29 +0300

    feat: implement toReadable function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function toReadable (number) {
-  
+    let mas  =  ['','one','two','three','four','five', 'six', 'seven', 'eight', 'nine', 'ten',
+                'eleven','twelve','thirteen','fourteen','fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty'
+                ];
+    let dec_mas  =  ['','ten','twenty','thirty','forty','fifty', 'sixty', 'seventy', 'eighty', 'ninety', 'one hundred'];  
+    
+    let nemberMas = number.toString().split('');
+    let str = '';
+    if (number == 0) return 'zero';
+    else if (mas[number]) return mas[number];
+    else if (dec_mas[number/10]) return dec_mas[number/10];
+    else if (nemberMas.length == 2) {
+        str+= dec_mas[nemberMas[0]] + ' ' + mas[nemberMas[1]];
+        return ('str = **', str.trim()); //return
+    }else if (nemberMas.length == 3){
+        str+= mas[nemberMas[0]] + ' hundred' +  ' ' + (mas[number%100] || dec_mas[nemberMas[1]] + ' ' + mas[nemberMas[2]]);
+        return ('str = ***', str.trim()); //return
+    }
 }
